dd90913ba04d08239a5e3687d701f053f06a65b0
OrionXV <syedarsalaan12@gmail.com>
2023-03-17 15:45:51 +0530

    dot and animation fix

--- assets/js/styleJS.js
+++ assets/js/styleJS.js
@@ -11,17 +11,43 @@ $(document).ready(function(){
 
             $('html, body').animate({
                 scrollTop: $(hash).offset().top
-            }, 700, function(){
+            }, 800, function(){
                 window.location.hash = hash;
             });
         } 
     });
 });
 
+$(document).ready(function(){
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+  
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+  
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+    });
+  });
+
+  
 // portfolio carousel
 $('#owl-portfolio').owlCarousel({
     margin:30,
-    dots: false,
+    dots: true,
     responsiveClass:true,
     responsive:{
         0:{
